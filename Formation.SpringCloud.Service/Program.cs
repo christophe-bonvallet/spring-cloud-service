62eb45ea6d7dc52a53d13dc1ef3b41e2862ca399
Christophe Bonvallet <christophe.bonvallet@photoweb.fr>
2020-06-30 15:13:28 +0200

    feature : implements config server

--- Formation.SpringCloud.Service/Program.cs
+++ Formation.SpringCloud.Service/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Steeltoe.Common.Hosting;
 using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Steeltoe.Extensions.Configuration.ConfigServer;
 using Steeltoe.Extensions.Logging.DynamicLogger;
 
 namespace Formation.WeatherForecast.Service
@@ -27,6 +28,7 @@ namespace Formation.WeatherForecast.Service
                 })
                 .UseCloudHosting(9339)
                 .AddCloudFoundry()
-                .AddDynamicLogging();
+                .AddDynamicLogging()
+                .AddConfigServer();
     }
 }
--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,8 +2,11 @@
 
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
+pushd ${DIR}/..
 
 dotnet publish
 
 cf push Formation.SpringCloud.Service     -f ./ci/manifest.yml -p ./Formation.SpringCloud.Service/bin/Debug/netcoreapp3.1/publish --random-route # random-route is here for dev purpose
 
+
+popd
